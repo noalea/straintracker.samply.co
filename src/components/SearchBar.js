4cbb63ee1796bf14bbab67b214d3460c8aedd582
Noa Gorelik <noagorelik@yahoo.ca>
2018-08-06 22:56:20 -0400

    update path on search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import searchImg from '../assets/search.png';
+import $ from 'jquery';
 
 class SearchBar extends Component {
 
   state = {
   };
 
-  componentDidMount() {
+  typing() {
+    let val, link;
+    val = $(".search-bar input").val();
+    link = "/search/" + val;
+    this.props.history.push(link);
   };
 
   render() {
     return (
       <div className="search-bar">
         <img src={searchImg} />
-        <input type={'text'} placeholder={'Search a Strain...'} />
+        <input type={'text'} onChange={this.typing.bind(this)} placeholder={'Search a Strain...'} />
       </div>
     );
   }
 }
 
-export default SearchBar;
+export default withRouter(SearchBar);
